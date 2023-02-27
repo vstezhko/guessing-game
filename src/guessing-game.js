4903c5eddb2fb27d111f3f3c1f8e4a28c1e263c5
vika <viktoriastezhko@gmail.com>
2023-02-27 15:56:34 +0300

    add result

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.ceil((this.min + this.max) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate;
     }
 
     greater() {
-
+        this.min = this.candidate;
     }
 }
 
